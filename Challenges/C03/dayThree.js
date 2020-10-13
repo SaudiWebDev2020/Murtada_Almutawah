6a021e8f2185e08c88f0f0343cfc3711f337cd03
Murtada Almutawah <mamutawah@gmail.com>
2020-10-13 10:28:07 +0300

    Fix a bug in removeNegatives

--- Challenges/C03/dayThree.js
+++ Challenges/C03/dayThree.js
@@ -8,11 +8,11 @@
 function reverseArray(arr) {
     var r = arr.length-1;
     for (var x = 0; x < arr.length; x++){
-        if (x != r && x < r) {
+        if (x < r) {
             temp = arr[x];
             arr[x] = arr[r];
             arr[r] = temp;
-            console.log(arr, x, r);
+            //console.log(arr, x, r);
             r--;
         }
     }
@@ -20,11 +20,11 @@ function reverseArray(arr) {
     return arr;
 }
 
-arr1 = [1, -3, 0,4];
-// 20, 0, 100, 7, -11 , 0, -3, 1
-//console.log(arr1);
-reverseArray(arr1);
-console.log(arr1);
+// arr1 = [1, -3, 0,4];
+// // 20, 0, 100, 7, -11 , 0, -3, 1
+// //console.log(arr1);
+// reverseArray(arr1);
+// console.log(arr1);
 
 // Remove Negatives
 // -----------------------------------------------------
@@ -36,22 +36,20 @@ console.log(arr1);
 function removeNegatives(arr) {
     var count = 0;
     for (var trac = 0; trac < arr.length; trac++){
-        if (arr[trac] < 0) {
+        if (arr[trac] >= 0) {
+            arr[trac - count] = arr[trac];
+        } else {
             count++;
-        }
-        arr[trac] = arr[trac + count];
+        }   
     }
     //console.log(arr,count);
     arr.length -= count;
     return arr;
 }
 
-
-//arr1 = [1, -3, 0, -11];
-//removeNegatives(arr1);
-//console.log(arr1);
-
-
+arr1 = [1, -3, 0,5, -11,-10,9];
+removeNegatives(arr1);
+console.log(arr1);
 
 // Skyline Heights
 // -----------------------------------------------------
@@ -59,13 +57,12 @@ function removeNegatives(arr) {
 // represent three buildings: first is actually below street level, second is seven stories high, and third is
 // three stories high (but hidden behind the seven-story onbe). You are situated at street level. Return an
 // array containing heights of the buildings you can see, in order. Given [1,-1,7,3] return [1,7].
-
 function skylineHeights(arr) {
-    var max = 0;
-    var count = 0;
+    var max = 0; // init lock lvl 
+    var count = 0; // counter for how many buldings are hidden 
     for (var trac = 0; trac < arr.length; trac++){
         if (arr[trac] < max) {
-            count++;
+            count++; 
         } else {
             max = arr[trac];
             arr[trac - count] = arr[trac];
@@ -75,7 +72,6 @@ function skylineHeights(arr) {
     arr.length -= count;
     return arr;
 }
-
 //arr1 = [1, -3, 0, -11,7,100, 0, 20];
 //skylineHeights(arr1);
 //console.log(arr1);
