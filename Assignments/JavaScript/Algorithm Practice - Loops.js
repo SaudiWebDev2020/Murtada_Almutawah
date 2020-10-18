c3a77bdfaadc5bed2ac37913ccc4973d8e8f4eda
Murtada Almutawah <mamutawah@gmail.com>
2020-10-18 09:49:45 +0300

    Finishing Loop Assignment

---
+++ Assignments/JavaScript/Algorithm Practice - Loops.js
@@ -0,0 +1,15 @@
+
+
+// Print Odds 1 - 20 
+for (var i = 0; i <= 20; i++){
+    if (i % 2 == 1) {
+        console.log(i);
+    }
+}
+
+// Sum and print 1 - 5 
+var sum = 0;
+for (var j = 1; j <= 5; j++){
+    sum += j;
+    console.log("Num: ", j, "Sum: ", sum);
+}
