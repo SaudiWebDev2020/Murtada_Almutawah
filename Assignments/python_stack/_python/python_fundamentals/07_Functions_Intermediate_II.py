612ef56608074addb630a45a69d79fcf621bd834
Murtada Almutawah <mamutawah@gmail.com>
2020-10-26 13:29:47 +0300

    finish functions intermediate II

--- Assignments/python_stack/_python/python_fundamentals/07_Functions_Intermediate_II.py
+++ Assignments/python_stack/_python/python_fundamentals/07_Functions_Intermediate_II.py
@@ -29,6 +29,9 @@ z[0]['y'] = 30
 # print(z[0]['y'])
 
 
+# 2. Iterate Through a List of Dictionaries
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+
 def iterateDictionary(some_list):
     for items in some_list:
         string = ''
@@ -54,5 +57,50 @@ iterateDictionary(students)
 # first_name - KB, last_name - Tonel
 
 
+# DONE 3. Get Values From a List of Dictionaries
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
 def iterateDictionary2(key_name, some_list):
-    pass
+    for items in some_list:
+        print(items[key_name])
+
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+
+# 4. Iterate Through a Dictionary with List Values
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
+
+def printInfo(some_dict):
+    for item_list in some_dict:
+        print(len(some_dict[item_list]), item_list)
+        for item in some_dict[item_list]:
+            print(item)
+        # print('\n')
+    # print(some_dict)
+
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+# Expected output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
