7038eea1eef6d0129e936d231f1d3910e5e7d92c
Murtada Almutawah <mamutawah@gmail.com>
2020-10-31 02:02:58 +0300

    finish part of dojo fruit store

---
+++ Assignments/python_stack/flask/flask_fundamentals/dojo_fruit_store/server.py
@@ -0,0 +1,37 @@
+import datetime
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    print(request.form)
+    obj = {
+        'strawberry': int(request.form['strawberry']),
+        'raspberry': int(request.form['raspberry']),
+        'apple': int(request.form['apple']),
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'student_id': request.form['student_id']
+    }
+    customerName = f"{obj['first_name']} {obj['last_name']}"
+    count = obj['strawberry'] + obj['raspberry'] + obj['apple']
+    date = datetime.datetime.now().strftime("%B %d %Y %I:%M:%S %p")
+    print(f'Charging {customerName} for {count} fruits"')
+    print(date)
+
+    return render_template("checkout.html", checkout_details=obj, ordered_count=count, date_of_charge=date)
+
+
+@app.route('/fruits')
+def fruits():
+    return render_template("fruits.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
