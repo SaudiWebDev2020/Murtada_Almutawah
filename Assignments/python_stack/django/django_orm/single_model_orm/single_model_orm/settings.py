2262254db4eed9d787783f9b3bbcf05426f16cb6
Murtada Almutawah <mamutawah@gmail.com>
2020-11-03 13:02:10 +0300

    remove securety key

--- Assignments/python_stack/django/django_orm/single_model_orm/single_model_orm/settings.py
+++ Assignments/python_stack/django/django_orm/single_model_orm/single_model_orm/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&ljn-r_k@66c)46v%w)45_a0l^!ipptm@d7oyt8et22x8b*9zd'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
