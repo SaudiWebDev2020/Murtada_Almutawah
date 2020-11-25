c3c79e60f32d2eeb8a5f98e51297b6aaf40472d7
Murtada Almutawah <mamutawah@gmail.com>
2020-11-26 02:07:09 +0300

    prop it up assignment

---
+++ Assignments/mern_stack/react/class_components/prop_it_up/src/Components/PersonCard.jsx
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class PersonCard extends Component {
+    state = {  }
+    render() { 
+        return (
+        <div>
+        <h1> {this.props.lastName}, {this.props.firstName}</h1>
+        <p>Age: {this.props.age} </p>
+        <p>Hair Color: {this.props.hairColor} </p>
+        </div>
+        );
+    }
+}
+ 
+export default PersonCard;
