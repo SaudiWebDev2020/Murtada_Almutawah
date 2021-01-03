388d2ae6821ad03edca89a2fdf8339459679a72e
Murtada Almutawah <mamutawah@gmail.com>
2021-01-03 17:55:00 +0300

    FizzBuzz

---
+++ Assignments/java_stack/_java/java_fundamentals/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+
+public class FizzBuzz {
+  public String fizzBuzz(int number) {
+    // fizzbuzz logic here
+    if (number % 3 == 0 && number % 5 == 0) {
+      return "FizzBuzz";
+    } else if (number % 3 == 0) {
+      return "Fizz";
+    } else if (number % 5 == 0) {
+      return "Buzz";
+
+    } else {
+      return "" + number;
+    }
+  }
+}
---
+++ Assignments/java_stack/_java/java_fundamentals/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+
+public class FizzBuzzTest {
+  public static void main(String[] args) {
+    FizzBuzz fBuzz = new FizzBuzz();
+    System.out.println(fBuzz.fizzBuzz(3));
+    System.out.println(fBuzz.fizzBuzz(5));
+    System.out.println(fBuzz.fizzBuzz(15));
+    System.out.println(fBuzz.fizzBuzz(2));
+
+  }
+}
